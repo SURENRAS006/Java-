5985790cef7ecab5e3ca23e7443407a887cf181c
SURENRA S <surenra006@gmail.com>
2025-08-11 14:33:01 +0530

    Add files via upload

---
+++ AdjListDriver.java
@@ -0,0 +1,20 @@
+package Graph;
+
+public class AdjListDriver {
+	public static void main(String[] args) {
+		AdjList al=new AdjList(8);
+		al.addEdge(0, 1);
+        al.addEdge(0, 4);
+        al.addEdge(0, 6);
+        al.addEdge(1, 2);
+        al.addEdge(1, 5);
+        al.addEdge(1, 7);
+        al.addEdge(2, 7);
+        al.addEdge(3, 2);
+        al.addEdge(4, 5);
+        al.addEdge(4, 3);
+        al.printList();
+        al.BFS(0);
+        al.DFS(0);
+	}
+} 
